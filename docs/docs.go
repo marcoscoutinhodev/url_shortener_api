a91e7442db595a2295d39b4b7819d5d001b598ca
marcoscoutinhodev@outlook.com <marcoscoutinhodev@outlook.com>
2023-06-18 15:56:40 -0300

    docs: update

--- docs/docs.go
+++ docs/docs.go
@@ -429,7 +429,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "0.1",
+	Version:          "1.0.0",
 	Host:             "localhost:4001",
 	BasePath:         "",
 	Schemes:          []string{},
